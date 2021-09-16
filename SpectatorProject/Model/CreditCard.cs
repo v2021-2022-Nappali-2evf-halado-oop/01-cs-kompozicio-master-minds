b399b6bf0f5e66390262a27e07fdd582119f3323
sz0barmar <sz0barmar@vasvari.org>
2021-09-16 12:27:32 +0200

    CreditCard osztály

---
+++ SpectatorProject/Model/CreditCard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpectatorProject.Model
+{   /// <summary>
+    /// Hitelkártya osztály
+    /// </summary>
+    class CreditCard
+    {
+        
+    }
+}
