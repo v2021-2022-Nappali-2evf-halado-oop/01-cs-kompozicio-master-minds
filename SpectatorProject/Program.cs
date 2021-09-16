8df991e7b3303fe6da4ac07a9b992ab697fd0b7b
krisz501@sulid.hu <krisz501@sulid.hu>
2021-09-16 12:20:03 +0200

    Mapparendszer létrejötte

--- SpectatorProject/Program.cs
+++ SpectatorProject/Program.cs
@@ -6,7 +6,7 @@ namespace SpectatorProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
