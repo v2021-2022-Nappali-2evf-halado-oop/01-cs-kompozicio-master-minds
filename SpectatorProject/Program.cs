837627a411e54fd5b44e1daa21050c08c22ab44d
krisz501@sulid.hu <krisz501@sulid.hu>
2021-09-21 11:16:47 +0200

    Tesztkód

--- SpectatorProject/Program.cs
+++ SpectatorProject/Program.cs
@@ -6,7 +6,35 @@ namespace SpectatorProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Name name = new Name("László", "Látó");
+            Console.WriteLine("Teljes név tulajdonság: " + name.FullName);
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("hu-HU");
+            DateTime dt = new DateTime(2022, 3, 15);
+            string dateString = dt.ToString();
+            CreditCard creditCard = new CreditCard("11", dt);
+
+            if (creditCard.Expired)
+                Console.WriteLine("Lejárat tulajdonság: a hitelkártya lejárt");
+            else
+                Console.WriteLine("Lejárat tulajdonság: a hitelkártya érvényes");
+
+            Status status = Status.Active;
+            BonusCard bonusCard = new BonusCard("12", status, 30000);
+
+            if (bonusCard.Usable)
+                Console.WriteLine("Próba: a bónuszkártya használható");
+            else
+            {
+                Console.WriteLine("A bonuszkártya nem használható!");
+            }
+            Spectator spectator = new Spectator(name, creditCard, bonusCard);
+            Console.WriteLine(spectator);
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
